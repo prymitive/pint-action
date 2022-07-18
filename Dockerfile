d9db9a565f6dfa75b2e1d90414df2457ada4d5c4
Łukasz Mierzwa <l.mierzwa@gmail.com>
2022-07-18 14:03:21 +0100

    Use pint v0.25.0

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM ghcr.io/cloudflare/pint:0.24.1
+FROM ghcr.io/cloudflare/pint:0.25.0
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,6 @@ else
     CMD="ci"
     BASEBRANCH="$GITHUB_BASE_REF"
     PRBRANCH="$GITHUB_HEAD_REF"
-    PRNUMBER=$(echo "$GITHUB_REF" | awk -F / '{print $3}')
     echo ">>> BASE BRANCH: $BASEBRANCH"
     echo ">>> PR BRANCH: $PRBRANCH"
     echo ">>> PR NUMBER: $PRNUMBER"
@@ -24,5 +23,5 @@ else
     git fetch origin "$PRBRANCH"
     git checkout "$PRBRANCH"
 
-    env GITHUB_PULL_REQUEST_NUMBER="$PRNUMBER" pint $CONFIG $LOGLEVEL ci --base-branch="$BASEBRANCH"
+    pint $CONFIG $LOGLEVEL ci --base-branch="$BASEBRANCH"
 fi
