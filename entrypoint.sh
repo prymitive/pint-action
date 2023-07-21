a8dbcb290b7d1a99509334af98af3ed3a1371d91
Will Bollock <wbollock@linode.com>
2023-07-21 12:35:16 -0400

    feat: support forks
    todo add info

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,18 +20,46 @@ if [ "$GITHUB_EVENT_NAME" != "pull_request" ]; then
     pint $CONFIG $LOGLEVEL lint $MIN_SEVERITY $REQUIRE_OWNER $WORKDIR
 else
     CMD="ci"
+    git config --global --add safe.directory ${GITHUB_WORKSPACE}
     BASEBRANCH="$GITHUB_BASE_REF"
     PRBRANCH="$GITHUB_HEAD_REF"
-    echo ">>> BASE BRANCH: $BASEBRANCH"
-    echo ">>> PR BRANCH: $PRBRANCH"
 
-    echo ">>> GITHUB_WORKSPACE: $GITHUB_WORKSPACE"
-    git config --global --add safe.directory ${GITHUB_WORKSPACE}
-    
-    git fetch origin "$BASEBRANCH"
-    git checkout "$BASEBRANCH"
-    git fetch origin "$PRBRANCH"
-    git checkout "$PRBRANCH"
+    if [ "${PULL_REQUEST_TARGET_REPO}" != "${PULL_REQUEST_SOURCE_REPO}" ]; then
+    # pull request is from fork
+        git config --global user.email "pint@fakemail.com"
+        git config --global user.name "pint"
+        git config pull.rebase false
+        # # https://github.com/reg-viz/reg-suit#workaround-for-detached-head
+        # # https://github.com/reg-viz/reg-suit/issues/590#issuecomment-1219155722
+        # workaround for detached head
+        git checkout ${PRBRANCH}
+        git branch --set-upstream-to=origin/main ${PRBRANCH}
 
-    pint $CONFIG $LOGLEVEL ci --base-branch="$BASEBRANCH" $REQUIRE_OWNER
+        git pull
+
+        # populate a local main branch with upstream data
+        git checkout remotes/origin/main
+        git branch main
+        git reset --hard remotes/origin/main
+        git remote -v
+
+        # go back to our fork branch
+        # git checkout ${GITHUB_HEAD_REF#refs/heads/}
+        git checkout "$PRBRANCH"
+    else
+    # pull request is not from fork
+
+
+      echo ">>> BASE BRANCH: $BASEBRANCH"
+      echo ">>> PR BRANCH: $PRBRANCH"
+
+      echo ">>> GITHUB_WORKSPACE: $GITHUB_WORKSPACE"
+
+      git fetch origin "$BASEBRANCH"
+      git checkout "$BASEBRANCH"
+      git fetch origin "$PRBRANCH"
+      git checkout "$PRBRANCH"
+    fi
+    pint $CONFIG $LOGLEVEL $CMD --base-branch="$BASEBRANCH" $REQUIRE_OWNER
+
 fi
